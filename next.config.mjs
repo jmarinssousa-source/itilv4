f1fe85a5c72d30fcef823ac29c03461167cc8d2f
JONATA MARINS SOUSA <jonata.sousa@fiocruz.br>
2026-06-16 16:42:03 -0300

    Deploy app with GitHub Pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,11 @@
 /** @type {import('next').NextConfig} */
+const isGitHubPages = process.env.GITHUB_PAGES === "true";
+
 const nextConfig = {
+  output: "export",
+  trailingSlash: true,
+  basePath: isGitHubPages ? "/itilv4" : "",
+  assetPrefix: isGitHubPages ? "/itilv4/" : "",
   typedRoutes: true
 };
 
